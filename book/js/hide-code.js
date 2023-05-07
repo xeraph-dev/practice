477cae5fb36ba0d47ae9c3c9ccd79a7d45e331a7
Xeraph <61700204+adrianrl99@users.noreply.github.com>
2023-05-07 05:08:51 +0000

    fix(book): fixed hide code script

--- book/js/hide-code.js
+++ book/js/hide-code.js
@@ -1,26 +1,15 @@
-function hideCode(hide) {
-  const els = document.querySelectorAll('a[href$="/solution.html"]')
-  for (let el of els) {
-    el = el.parentElement.parentElement.parentElement
-    if (hide) el.style.display = 'none'
-    else el.style.display = null
-  }
-}
-
 var container = document.querySelector('#menu-bar > .right-buttons')
 
 var label = document.createElement('label')
 label.innerText = 'Solutions'
+label.id = 'hide-code'
 
 var input = document.createElement('input')
 input.type = 'checkbox'
-input.id = 'hide-code'
 input.hidden = true
 input.checked = localStorage.getItem('hide-code') === 'true' ?? true
-hideCode(input.checked)
 input.onchange = e => {
   localStorage.setItem('hide-code', e.target.checked)
-  hideCode(e.target.checked)
 }
 
 label.appendChild(input)
--- book/src/tutorials/overview.md
+++ book/src/tutorials/overview.md
@@ -1,3 +1,3 @@
 # Tutorials Overview
 
-This section has some short, concise tutorials on languages/frameworks/libraries that I'll be using in practice.s
+This section has some short, concise tutorials on languages/frameworks/libraries that I'll be using in practice.
